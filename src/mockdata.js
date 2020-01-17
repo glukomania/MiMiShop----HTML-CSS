5049d2ab309dfe1f2658b58378b718694071c2a3
glukomania <glukomani@gmail.com>
2020-01-17 15:04:19 +0100

    fixed images? Maybe

--- src/mockdata.js
+++ src/mockdata.js
@@ -5,7 +5,7 @@ export const mockProducts = [
     title: `The Rabbit Bob`,
     parameters: `Height 30cm, weight 200g`,
     price: `15€`,
-    image: `../img/photo-1-desktop.jpg`
+    image: `./img/photo-1-desktop.jpg`
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const mockProducts = [
     title: `The Bear Brayan`,
     parameters: `Height 20cm, weight 100g`,
     price: `10€`,
-    image: `../img/photo-2.jpg`
+    image: `./img/photo-2.jpg`
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const mockProducts = [
     title: `The Fox Nanny`,
     parameters: `Height 22cm, weight 140g`,
     price: `18€`,
-    image: `../img/photo-3.jpg`
+    image: `./img/photo-3.jpg`
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const mockProducts = [
     title: `The owl baskets`,
     parameters: `Radius 20cm, weight 100g, 2 items`,
     price: `26€`,
-    image: `../img/photo-4.jpg`
+    image: `./img/photo-4.jpg`
   },
   {
     id: 6,
@@ -37,7 +37,7 @@ export const mockProducts = [
     title: `Pens saver`,
     parameters: `Width 25cm, height 22 cm, for 10 pens`,
     price: `31€`,
-    image: `../img/photo-5.jpg`
+    image: `./img/photo-5.jpg`
   },
   {
     id: 7,
@@ -45,7 +45,7 @@ export const mockProducts = [
     title: `Candle warmer`,
     parameters: `Height 12 cm`,
     price: `15€`,
-    image: `../img/photo-6.jpg`
+    image: `./img/photo-6.jpg`
   },
   
 ]
